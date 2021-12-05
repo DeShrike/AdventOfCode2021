4bf08e12675e04a762b4f9e37e2b38d7baf4d1b4
Shrike <de_shrike@hotmail.com>
2021-12-05 15:19:07 +0100

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,7 +1,7 @@
 from math import gcd
 import fcntl, struct, termios
 
-def modify_bit(self, number: int, bit: int, value: int) -> int:
+def modify_bit(self, number:int, bit: int, value: int) -> int:
 	m = 1 << bit
 	return (number & ~m) | ((value << bit) & m)
 
@@ -10,3 +10,24 @@ def lcm(a: int, b: int) -> int:
 
 def terminal_size():
     return struct.unpack('HHHH', fcntl.ioctl(0, termios.TIOCGWINSZ, struct.pack('HHHH', 0, 0, 0, 0)))[:2]
+
+def dirange(start, end=None, step:int=1):
+    """
+    Directional, inclusive range. This range function is an inclusive version of
+    :class:`range` that figures out the correct step direction to make sure that it goes
+    from `start` to `end`, even if `end` is before `start`.
+    >>> dirange(2, -2)
+    [2, 1, 0, -1, -2]
+    >>> dirange(-2)
+    [0, -1, -2]
+    >>> dirange(2)
+    [0, 1, 2]
+    """
+    assert step > 0
+    if end is None:
+        start, end = 0, start
+
+    if end >= start:
+        yield from range(start, end + 1, step)
+    else:
+        yield from range(start, end - 1, step=-step)
