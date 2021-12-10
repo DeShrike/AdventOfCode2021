3861124a590e68f60d385fc8d0724eac5e3758f0
Shrike <de_shrike@hotmail.com>
2021-12-10 11:38:27 +0100

    Create PNG for day 9 puzzle

--- day9.py
+++ day9.py
@@ -1,4 +1,5 @@
 from aoc import Aoc
+from canvas import Canvas
 import itertools
 import math
 import re
@@ -45,7 +46,7 @@ class FloodFill():
             if not increased:
                 break
 
-        return len(self.points)
+        return self.points
 
 
 class Day9Solution(Aoc):
@@ -84,6 +85,27 @@ class Day9Solution(Aoc):
         self.inputdata.clear()
         self.TestDataA()
         return 1134
+
+    def CreatePNG(self, cellsize:int = 1):
+        print("Creating PNG")
+        width = len(self.inputdata[0]) * cellsize
+        height = len(self.inputdata) * cellsize
+        canvas = Canvas(width, height)
+
+        ff = FloodFill(self.inputdata)
+        lowpoints = self.FindLowPointsB()
+        for lp in lowpoints:
+            fieldpoints = ff.Run(*lp)
+            for fp in fieldpoints:
+                h = int(self.inputdata[fp[1]][fp[0]]) + 1
+                self.SetPixel(canvas, cellsize, *fp, (255 // h, 255 // h, 255 // h))
+            self.SetPixel(canvas, cellsize, *lp, (255, 255, 255))
+        canvas.save_PNG("day9.png")
+
+    def SetPixel(self, canvas, cellsize:int, x:int, y:int, color):
+        for xx in range(x * cellsize, (x + 1) * cellsize + 1):
+            for yy in range(y * cellsize, (y + 1) * cellsize + 1):
+                canvas.set_pixel(xx, yy, color)
 
     def GetNeighbours(self, x:int, y:int):
         n = []
@@ -133,11 +155,12 @@ class Day9Solution(Aoc):
 
         ff = FloodFill(self.inputdata)
         lowpoints = self.FindLowPointsB()
-        sizes = list(sorted([ff.Run(*lp) for lp in lowpoints], reverse=True))
+        sizes = list(sorted([len(ff.Run(*lp)) for lp in lowpoints], reverse=True))
 
         answer = sizes[0] * sizes[1] * sizes[2]
 
         self.ShowAnswer(answer)
+        self.CreatePNG(3)
 
 
 if __name__ == "__main__":
