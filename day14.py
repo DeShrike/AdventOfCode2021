6675207df07ec15e9eb4921577a93c741159a44d
Shrike <de_shrike@hotmail.com>
2021-12-14 20:21:35 +0100

    Day 14 done

--- day14.py
+++ day14.py
@@ -31,24 +31,24 @@ class Day14Solution(Aoc):
         self.inputdata.clear()
         testdata = \
         """
-		NNCB
+        NNCB
 
-		CH -> B
-		HH -> N
-		CB -> H
-		NH -> C
-		HB -> C
-		HC -> B
-		HN -> C
-		NN -> C
-		BH -> H
-		NC -> B
-		NB -> B
-		BN -> B
-		BB -> N
-		BC -> B
-		CC -> N
-		CN -> C
+        CH -> B
+        HH -> N
+        CB -> H
+        NH -> C
+        HB -> C
+        HC -> B
+        HN -> C
+        NN -> C
+        BH -> H
+        NC -> B
+        NB -> B
+        BN -> B
+        BB -> N
+        BC -> B
+        CC -> N
+        CN -> C
         """
         self.inputdata = [line.strip() for line in testdata.strip().split("\n")]
         return 1588
@@ -59,39 +59,93 @@ class Day14Solution(Aoc):
         return 2188189693529
 
     def ParseInput(self):
-    	template = self.inputdata[0]
-    	rules = {}
-    	for line in self.inputdata[2:]:
-    		parts = line.split(" -> ")
-    		rules[parts[0]] = parts[1]
-    	return template, rules
+        template = self.inputdata[0]
+        rules = {}
+        for line in self.inputdata[2:]:
+            parts = line.split(" -> ")
+            rules[parts[0]] = parts[1]
+        return template, rules
 
     def DoStep(self, template:str, rules) -> str:
-    	newtemplate = ""
+        newtemplate = ""
 
-    	for ix in range(len(template) - 1):
-    		newtemplate += template[ix] + rules[template[ix:ix + 2]]
+        for ix in range(len(template) - 1):
+            newtemplate += template[ix] + rules[template[ix:ix + 2]]
 
-    	return newtemplate + template[-1]
+        return newtemplate + template[-1]
 
     def PartA(self):
         self.StartPartA()
 
         template, rules = self.ParseInput()
         for _ in range(10):
-        	template = self.DoStep(template, rules)
+            template = self.DoStep(template, rules)
 
         counts = sorted(Counter(template).values())
         answer = counts[-1] - counts[0]
 
         self.ShowAnswer(answer)
 
+    def MakePairs(self, template:str):
+        pairs = {}
+        for ix in range(len(template) - 1):
+            pair = template[ix:ix + 2]
+            if pair in pairs:
+                pairs[pair][0] += 1
+                pairs[pair][1] += ix == len(template) - 2
+            else:
+                pairs[pair] = (1, ix == len(template) - 2)
+        return pairs
+
+    def SplitPair(self, key, count, rule, islast:bool, newpairs):
+        # print(f"Key: {key}  Count: {count}  Rule: {rule} IsLast: {islast}")
+        newkeyleft = key[0] + rule
+        newkeyright = rule + key[1]
+        # print(f"{newkeyleft} - {newkeyright}")
+        if newkeyleft in newpairs:
+            newpairs[newkeyleft] = (newpairs[newkeyleft][0] + count, False)
+        else:
+            newpairs[newkeyleft] = (count, False)
+
+        if newkeyright in newpairs:
+            newpairs[newkeyright] = (newpairs[newkeyright][0] + count, islast)
+        else:
+            newpairs[newkeyright] = (count, islast)
+
+    def DoStepEx(self, pairs, rules):
+        newpairs = {}
+        for key, data in pairs.items():
+            if key in rules:
+                count = data[0]
+                islast = data[1]
+                self.SplitPair(key, count, rules[key], islast, newpairs)
+        return newpairs
+
     def PartB(self):
         self.StartPartB()
 
-        # Add solution here
+        template, rules = self.ParseInput()
+        pairs = self.MakePairs(template)
+        for _ in range(40):
+            pairs = self.DoStepEx(pairs, rules)
 
-        answer = None
+        counts = {}
+        for pair, data in pairs.items():
+            l = pair[0]
+            r = pair[1]
+            count = data[0]
+            islast = data[1]
+            if l in counts:
+                counts[l] += count
+            else:
+                counts[l] = count
+            if islast:
+                if r in counts:
+                    counts[r] += count
+                else:
+                    counts[r] = count
+        sortedcounts = sorted(counts.values())
+        answer = sortedcounts[-1] - sortedcounts[0] + 1
 
         self.ShowAnswer(answer)
 
